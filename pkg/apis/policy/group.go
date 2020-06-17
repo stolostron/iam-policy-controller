b2dc0a3fbda319fb423cac95914a7321644794e6
ckandag <ckandaga@redhat.com>
2020-06-17 09:13:04 -0500

    copyright

--- pkg/apis/policy/group.go
+++ pkg/apis/policy/group.go
@@ -1,3 +1,10 @@
+// Licensed Materials - Property of IBM
+// (c) Copyright IBM Corporation 2018. All Rights Reserved.
+// Note to U.S. Government Users Restricted Rights:
+// Use, duplication or disclosure restricted by GSA ADP Schedule
+// Contract with IBM Corp.
+// Copyright (c) 2020 Red Hat, Inc.
+
 // Package policy contains policy API versions.
 //
 // This file ensures Go source parsers acknowledge the policy package
--- pkg/apis/policy/v1/doc.go
+++ pkg/apis/policy/v1/doc.go
@@ -1,3 +1,10 @@
+// Licensed Materials - Property of IBM
+// (c) Copyright IBM Corporation 2018. All Rights Reserved.
+// Note to U.S. Government Users Restricted Rights:
+// Use, duplication or disclosure restricted by GSA ADP Schedule
+// Contract with IBM Corp.
+// Copyright (c) 2020 Red Hat, Inc.
+
 // Package v1 contains API Schema definitions for the policy v1 API group
 // +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=github.com/open-cluster-management/iam-policy-controller/pkg/apis/policy
